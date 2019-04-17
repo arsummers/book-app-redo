95481cfe435e21f021870f9373454b1579af0f7b
Aliya Summers <aliyasummers1@gmail.com>
2019-04-16 22:37:53 -0700

    added dotenv, got server actually working

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
-/*
 //basic dependencies
+require('dotenv').config();
 const express = require('express');
 const superagent = require('superagent');
 
@@ -17,9 +17,10 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 //routes
-app.get('/', new_search);
+//renders page with search form
+app.get('/', newSearch);
 
-app.post('/searches', create_search);
+app.post('/searches', createSearch);
 
 //catches bad routes
 app.get('*', (request, response) => response.status(404).send('womp womp bad route'));
@@ -28,77 +29,15 @@ app.listen(PORT, () => console.log(`Book app listening on port ${3000}`));
 
 //helper functions
 function Book(info) {
+  const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
   this.title = info.title;
 }
 
-function new_search(request, response){
+function newSearch(request, response){
   response.render('pages/index');
 }
 
-function create_search(request, response){
-  let url='https://www.googleapis.com/books/v1/volumes?q=';
-
-  console.log(request.body);
-
-  if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
-
-  if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
-
-  console.log(url);
-
-  superagent.get(url)
-    .then(api_response = api_response.body.items.map(book_result => new Book(book_result.volume_info)))
-    .then(results => response.render('pages/searches/show', { search_results : results}));
-}
-
-*/
-//=================
-
-// Application Dependencies
-const express = require('express');
-const superagent = require('superagent');
-
-// Application Setup
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Application Middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-// Set the view engine for server-side templating
-app.set('view engine', 'ejs');
-
-
-// API Routes
-// Renders the search form
-app.get('/', newSearch);
-
-// Creates a new search to the Google Books API
-app.post('/searches', createSearch);
-
-// Catch-all
-app.get('*', (request, response) => response.status(404).send('This route does not exist'));
-
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
-
-// HELPER FUNCTIONS
-// Only show part of this to get students started
-function Book(info) {
-  const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
-
-  this.title = info.title || 'No Title Available';
-
-}
-
-// Note that .ejs file extension is not required
-function newSearch(request, response) {
-  response.render('pages/index');
-}
-
-// No API key required
-// Console.log request.body and request.body.search
-function createSearch(request, response) {
+function createSearch(request, response){
   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
 
   console.log(request.body);
@@ -110,9 +49,6 @@ function createSearch(request, response) {
   console.log(url);
 
   superagent.get(url)
-    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
-    .then(results => response.render('pages/searches/show', { searchResults: results }));
-  // how will we handle errors?
+    .then(apiResponse => apiResponse.body.items.map(book_result => new Book(book_result.volumeInfo)))
+    .then(results => response.render('pages/searches/show', { search_results : results}));
 }
-
-
